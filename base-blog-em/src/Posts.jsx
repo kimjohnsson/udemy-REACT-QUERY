827480b18af515e2b317f68e15e5bfa95568badd
Kim Johnsson <kim.johnsson@telia.com>
2025-02-18 10:47:58 +0100

    feat: error and loading state when fetching data

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -9,10 +9,23 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery({
+  const { data, isError, error, isLoading } = useQuery({
     queryKey: ['posts'],
     queryFn: fetchPosts
   });
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong...</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
