92ede54f2cdf78ca72ef0bee38567275d51353d4
Kim Johnsson <kim.johnsson@telia.com>
2025-02-18 11:34:54 +0100

    feat: fetching comments when selecting posts

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,21 +1,29 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { useQuery } from '@tanstack/react-query';
+import { fetchComments } from './api';
+import './PostDetail.css';
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['comments', post.id],
+    queryFn: () => fetchComments(post.id)
+  });
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>{error.toString()}</p>}
+      {!isLoading &&
+        !isError &&
+        data &&
+        data.map((comment) => (
+          <li key={comment.id}>
+            {comment.email}: {comment.body}
+          </li>
+        ))}
     </>
   );
 }
