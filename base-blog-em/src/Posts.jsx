57fbb9dd6a1ab6aa5ef680d4c61992744ac470c6
Kim Johnsson <kim.johnsson@telia.com>
2025-02-18 13:33:48 +0100

    feat: update post mutation

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -15,6 +15,10 @@ export function Posts() {
     mutationFn: (postId) => deletePost(postId)
   });
 
+  const updateMutation = useMutation({
+    mutationFn: (postId) => updatePost(postId)
+  });
+
   useEffect(() => {
     if (currentPage < maxPostPage) {
       const nextPage = currentPage + 1;
@@ -54,6 +58,7 @@ export function Posts() {
               className="post-title"
               onClick={() => {
                 deleteMutation.reset();
+                updateMutation.reset();
                 setSelectedPost(post);
               }}
             >
@@ -77,7 +82,13 @@ export function Posts() {
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} deleteMutation={deleteMutation} />}
+      {selectedPost && (
+        <PostDetail
+          post={selectedPost}
+          deleteMutation={deleteMutation}
+          updateMutation={updateMutation}
+        />
+      )}
     </>
   );
 }
