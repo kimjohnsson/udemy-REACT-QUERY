aac387a61b3c9632beeab01ae5bcac1100b17b98
Kim Johnsson <kim.johnsson@telia.com>
2025-02-18 11:43:41 +0100

    feat: enabling next and previous page buttons

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -6,12 +6,12 @@ import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   const { data, isError, error, isLoading } = useQuery({
-    queryKey: ['posts'],
-    queryFn: fetchPosts
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage)
   });
 
   if (isLoading) {
@@ -38,11 +38,17 @@ export function Posts() {
           ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage((currentPage) => currentPage - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => setCurrentPage((currentPage) => currentPage + 1)}
+        >
           Next page
         </button>
       </div>
